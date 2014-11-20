450c6b6a44223079586f51a76fb30f611b68c272
Lynn Martino <support@codiceebella.com>
2014-11-20 13:54:29 -0600

    Second commit - changed "Welcome" to "Home"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = 'd893ba5a4f0fd828af8da857ec68fcca76beaa3a93fd3281b57255b1c5b939eeea0f0223facba853ff1f41975e24d17bafc3412dd6ceaa82cacecec8b4a154ad'
